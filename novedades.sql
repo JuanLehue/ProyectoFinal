477cb7826a3f3d4b74218944e4a22839c75d0327
Juan Lehue <juancholehue@gmail.com>
2023-06-12 16:23:30 -0300

    modificacion de novedades parte 2

---
+++ novedades.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 12-06-2023 a las 19:22:36
+-- Versión del servidor: 8.0.31
+-- Versión de PHP: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyectoutn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `novedades`
+--
+
+DROP TABLE IF EXISTS `novedades`;
+CREATE TABLE IF NOT EXISTS `novedades` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
+  `subtitulo` text COLLATE utf8mb4_general_ci NOT NULL,
+  `cuerpo` text COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `novedades`
+--
+
+INSERT INTO `novedades` (`id`, `titulo`, `subtitulo`, `cuerpo`) VALUES
+(1, 'Nuevo lanzamiento de productos!!', 'Se viene los drops de invierno, mucha ropa nueva para pasar el frio.', 'LLega a nuestros locales los nuevos lanzamientos de la marca, con mucha ropa de invierno. Ropa super abrigada y comoda para disfrutar este invierno. Veni a cononocer todo lo nuevo en nuestro local.'),
+(3, 'Apertura de nuevo local!!\r\n', 'Se viene la inauguración del nuevo local de ropa de nuestra marca, junto a nuevas cosas que ya van a salir a la venta.', 'Estamos por abrir una sucursal nueva de nuestra marca, ya van a salir nuevas noticias. Tambien se vienen nuevos lanzamientos de muchas cosas y grandes ofertas.');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -15,6 +15,7 @@ router.get('/', async function(req, res, next) {
     });
 });
 
+// para eliminar novedad
 router.get('/eliminar/:id', async (req, res, next) => {
     const id = req.params.id;
     
@@ -22,6 +23,72 @@ router.get('/eliminar/:id', async (req, res, next) => {
     res.redirect('/admin/novedades')
 
 });
+
+// para q aparezca el form agregar
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    })//ciera render
+});//cierra get
+
+
+//para q guarde el agregar
+router.post('/agregar', async (req, res, next) => {
+    try {
+        if (req.body.titulo != "" && req.body.subtitulo != ""  && req.body.cuerpo != "") {
+            console.log(req.body)
+            await novedadesModel.insertNovedades(req.body);
+            res.redirect('/admin/novedades')
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargo la novedad'
+        })
+    }
+})
+
+//muestre el diseño del modificar con los datos de una novedad
+router.get('/modificar/:id', async (req, res, next) => {
+    var id = req.params.id;
+
+    var novedad = await novedadesModel.getNovedadById(id);
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        novedad
+    });//cierro render
+});//cierro get
+
+//modificacion de novedades
+router.post('/modificar', async (req, res, next) => {
+    try {
+        console.log(req.body.id);//para ver si trae el id
+        var obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo
+        }
+
+        console.log(obj)//para ver si trae los datos
+        await novedadesModel.modificarNovedadById(obj, req.body.id);
+        res.redirect('/admin/novedades');
+    } catch (error) {
+        console.log(error)
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se modifico la novedad'
+        })
+    }//cierro catch
+});//cierro post
 
 
 module.exports = router;
